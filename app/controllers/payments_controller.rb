8bacff01ab32a34b4b4bd368bf5aee5fdf259d29
Ilya Katz <ilyakatz@gmail.com>
2014-07-28 23:14:40 -0400

    ok, now payment is fixed

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,6 +8,7 @@ class PaymentsController < ApplicationController
   def create
     @contact = current_user.contacts.find(params[:contact_id])
     @payment_amount = params[:payment][:amount]
+    @coinbase = coinbase
     wallet = @contact.wallet
 
     @transaction = coinbase.send_money wallet, @payment_amount, "sending money through api"
